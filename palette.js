cb0c05d74a9f336aa9f6245afdf881302490cb31
Herman Stoud Platou <hermanp@stacc.com>
2020-09-15 12:04:44 +0200

    init

---
+++ palette.js
@@ -0,0 +1,12 @@
+nordea_palette = {
+  Nordea_blue: "#0000a0",
+  Light_background: "#e5f2f",
+  Vivid_blue: "#0000f",
+  Light_pink: "#fdece4",
+  Nordea_pink: "#fbd9ca",
+  Accent_green: "#40bfa3",
+  Accent_yellow: "#fe183",
+  White: "#fff",
+  Light_gray: " #e6e4e3",
+  Dark_gray: "#474748",
+};
---
+++ readme.md
@@ -0,0 +1,2 @@
+Reference the official branding guidelines for further details <https://www.nordea.lu/wp-content/uploads/2017/07/NORDEA.pdf>
+Other branding resources available at: <https://www.nordea.com/en/press-and-news/press-photos/nordea-master-brand/>
