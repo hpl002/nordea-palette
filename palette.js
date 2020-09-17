7dc2a325dda915bb67083c1190e10f3a779da21d
Herman Stoud Platou <hermanp@stacc.com>
2020-09-17 11:03:41 +0200

    palette update

--- palette.js
+++ palette.js
@@ -1,12 +1,21 @@
-nordea_palette = {
+const nordea_palette = {
   Nordea_blue: "#0000a0",
-  Light_background: "#e5f2f",
-  Vivid_blue: "#0000f",
+  Light_background: " #e5f2ff",
+  Vivid_blue: " #0000ff",
   Light_pink: "#fdece4",
   Nordea_pink: "#fbd9ca",
   Accent_green: "#40bfa3",
-  Accent_yellow: "#fe183",
+  Accent_yellow: " #ffe183",
   White: "#fff",
   Light_gray: " #e6e4e3",
   Dark_gray: "#474748",
+};
+
+const basic = {
+  Coal: "#2B2B2B",
+  Grey_1: "#6C6C6C",
+  Grey_2: "#B8B8B8",
+  Grey_3: "#E4E2E2",
+  Grey_4: "#F8F8F8",
+  Snow: "#FFFFFF",
 };
